cce73755c8d20f089f42dc5261b2b9be5bccfbd5
Kevin Chaparro <kevinchaparro@ElKevin2.local>
2024-12-17 10:10:41 -0700

    Modulo de Proveedores

--- Control_Almacen/src/controllers/proveedor.controller.js
+++ Control_Almacen/src/controllers/proveedor.controller.js
@@ -1,47 +1,103 @@
+const modelProveedor = require("../models/proveedor.model");
 
+const getProveedor = async (req, res) => {
+  try {
+    const response = await modelProveedor.findAll();
+    res.status(200).json({
+      ok: true,
+      data: response,
+    });
+  } catch (error) {
+    console.log("error", error);
+    res.status(501).json({
+      ok: false,
+      message: "Error al obtener proveedor",
+    });
+  }
+};
 
-const modelProveedor= require('../models/proveedor.model')
+const addProveedor = async (req, res) => {
+  if (Object.keys(req.body).length == 0) {
+    return res.status(404).json({
+      ok: false,
+      message: "No se recibieron datos",
+    });
+  }
+  try {
+    const response = await modelProveedor.create(req.body);
 
+    res.status(200).json({
+      ok: true,
+      message: "Proveedor creado con exito",
+    });
+  } catch (error) {
+    res.status(501).json({
+      ok: false,
+      message: "Error al agregar proveedor",
+    });
+  }
+};
 
-const getProveedor=async (req,res)=>{
-    try {
-        const response= await modelProveedor.findAll();
-        res.status(200).json({
-            ok:true,
-            data:response
-        })
-    } catch (error) {
-        console.log("error",error)
-        res.status(501).json({
-            ok:false,
-            message: "Error al obtener proveedor"
-        })
+const updateProveedor = async (req, res) => {
+  if (Object.keys(req.body).length == 0) {
+    return res.status(404).json({
+      ok: false,
+      message: "No se recibieron datos",
+    });
+  }
+  try {
+    const response = await modelProveedor.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (response[0] == 0) {
+      return res.status(404).json({
+        ok: false,
+        message: "Proveedor no encontrado",
+      });
     }
-}
+    res.status(200).json({
+      ok: true,
+      message: "Proveedor actualizado con exito",
+    });
+  } catch (error) {
+    res.status(501).json({
+      ok: false,
+      message: "Error al actualizar proveedor",
+    });
+  }
+};
 
-const addProveedor=async (req,res)=>{
-        if(Object.keys(req.body).length==0){
-            res.status(404).json({
-                ok:false,
-                message: "No se recibieron datos"
-            })
-        }
-    try {
-        const response= await modelProveedor.create(req.body);
-        res.status(200).json({
-            ok:true,
-            message:'Proveedor creado con exito'
-        })
-    } catch (error) {
-        res.status(501).json({
-            ok:false,
-            message: "Error al agregar proveedor"
-        })
+const deleteProveedor = async (req, res) => {
+  try {
+    const response = await modelProveedor.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (response == 0) {
+      return res.status(404).json({
+        ok: false,
+        message: "Proveedor no encontrado",
+      });
     }
-}
+    res.status(200).json({
+      ok: true,
+      message: "Proveedor eliminado con exito",
+    });
+  } catch (error) {
+    res.json({
+      ok: false,
+      message: "Error al eliminar proveedor",
+    });
+  }
+};
 
-
-module.exports={
-    getProveedor,
-    addProveedor
-}
+module.exports = {
+  getProveedor,
+  addProveedor,
+  updateProveedor,
+  deleteProveedor
+};
--- Control_Almacen/src/routes/proveedor.route.js
+++ Control_Almacen/src/routes/proveedor.route.js
@@ -1,11 +1,13 @@
 
 const {Router}= require('express');
-const { getProveedor, addProveedor } = require('../controllers/proveedor.controller');
+const { getProveedor, addProveedor, deleteProveedor, updateProveedor } = require('../controllers/proveedor.controller');
 
 const router= Router();
 
 
 router.get('',getProveedor)
 router.post('',addProveedor)
+router.delete('/:id',deleteProveedor)//Recibiendo por params
+router.patch('/:id',updateProveedor)
 
 module.exports=router
